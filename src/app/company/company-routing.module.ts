08a0cf609566ac8809d0645c806d7ecce17cb0c3
dinesh-kumar19 <dineshkumarsdk03@gmail.com>
2024-12-31 00:12:12 +0530

    worked with company module

--- src/app/company/company-routing.module.ts
+++ src/app/company/company-routing.module.ts
@@ -3,10 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { CompanyComponent } from './company.component';
 import { CompanyDashboardComponent } from './company-dashboard/company-dashboard.component';
 import { CompanyAuthGuard } from '../guards/company-auth.guard';
+import { CompanyProfileComponent } from './company-profile/company-profile.component';
+import { PostJobComponent } from './post-job/post-job.component';
+import { AllApplicationsComponent } from './all-applications/all-applications.component';
 
 const routes: Routes = [
-  { path: '', component: CompanyComponent },
-  { path: 'companyDashboard', component: CompanyDashboardComponent, canActivate: [CompanyAuthGuard]}
+  // { path: '', component: CompanyComponent },
+  { path: 'companyDashboard', component: CompanyDashboardComponent, canActivate: [CompanyAuthGuard]},
+  {path: 'company-profile', component: CompanyProfileComponent, canActivate: [CompanyAuthGuard]},
+  { path: 'post-job', component: PostJobComponent, canActivate: [CompanyAuthGuard]},
+  { path: 'applications', component: AllApplicationsComponent, canActivate: [CompanyAuthGuard]}
 ];
 
 @NgModule({
--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -5,13 +5,19 @@ import { CompanyRoutingModule } from './company-routing.module';
 import { CompanyComponent } from './company.component';
 import { CompanyDashboardComponent } from './company-dashboard/company-dashboard.component';
 import { CompanyHeaderComponent } from './company-header/company-header.component';
+import { CompanyProfileComponent } from './company-profile/company-profile.component';
+import { PostJobComponent } from './post-job/post-job.component';
+import { AllApplicationsComponent } from './all-applications/all-applications.component';
 
 
 @NgModule({
   declarations: [
     CompanyComponent,
     CompanyDashboardComponent,
-    CompanyHeaderComponent
+    CompanyHeaderComponent,
+    CompanyProfileComponent,
+    PostJobComponent,
+    AllApplicationsComponent
   ],
   imports: [
     CommonModule,
